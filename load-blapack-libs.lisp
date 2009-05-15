af61a43b10668f66494c17e182c0b2e240957b01
AJ Rossini <blindglobe@gmail.com>
2009-05-15 09:41:02 +0200

    added information from Leo regarding his experiments with loading without the gfortran libs on MacOSX
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- load-blapack-libs.lisp
+++ load-blapack-libs.lisp
@@ -9,7 +9,29 @@
 
 ;; EDIT THESE VARIABLES TO POINT TO YOUR LIBRARIES!
 
+
+#|
+;;; The suggestion (from Leo (sdl.web at gmail dot com) to use native
+;;; accelerated BLAS and LAPACK on MacOSX is to COMMENT OUT the
+;;; gfortran lib DEFPARAMETER and LOAD-FOREIGN-LIBRARY, as it is not
+;;; linked in, and to use the following locations:
+
+  ;; (defparameter *gfortran-lib* "/usr/lib/libgfortran.so.3")
+  (defparameter *blas-lib*
+     "/System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib")
+  (defparameter *lapack-lib*
+     "/System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.framework/Versions/A/libLAPACK.dylib")
+
+;;; and then for the load, comment it out:
+
++      ;; (load-foreign-library *gfortran-lib*)
+
+;;; This might not hold for your MacOSX setup, please report back!
+|#
+
+
 (eval-when (:compile-toplevel :load-toplevel)
+
   (defparameter *gfortran-lib* "/usr/lib/libgfortran.so.3")
   (defparameter *blas-lib* "/usr/lib/libblas.so")
   (defparameter *lapack-lib* "/usr/lib/liblapack.so")
